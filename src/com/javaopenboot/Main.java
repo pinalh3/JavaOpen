d1608323d44b5ed64633435f84318ba57e6d8aa8
pinalh3 <pinalh3@gmail.com>
2023-01-17 16:06:59 -0400

    Tipos de datos

---
+++ src/com/javaopenboot/Main.java
@@ -0,0 +1,31 @@
+package com.javaopenboot;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //numeros
+        byte num1=1;
+
+        short num2=2;
+
+        int num3=334;
+
+        long num4=34231;
+
+        float num5 = 3.4f;
+
+        double num6= 34.2;
+
+        //Texto
+
+        String t1="Hola";
+
+        char t2 = 'a';
+
+        //Logico
+
+        Boolean interes=true;
+
+        System.out.println("tipo byte "+num1+" tipo short "+num2+" tipo integer "+num3+" tipo long "+num4+" tipo float "+num5+" tipo double "+num6+ " tipo String "+t1+" tipo character "+t2+" tipo booleano "+interes);
+    }
+}
